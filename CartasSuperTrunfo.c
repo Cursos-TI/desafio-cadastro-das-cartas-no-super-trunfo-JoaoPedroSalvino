b35c7c23b10280a7be85451dcd588942f0b4fd2f
JoaoPedroSalvino <salvinov.jp@gmail.com>
2025-03-27 01:03:27 +0000

    Transcrevi o desafio completo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char estado[50];
+    char carta[10];
+    char cidade[50];
+    int pontos_turisticos;
+    int populacao;
+    float area_km2;
+    float PIB;
+
+    printf("Digite o Estado: \n");
+    scanf(" %s", estado);
+
+    printf("Digite o Código da Carta: \n");
+    scanf(" %s", carta);
+
+    printf("Digite o Nome da Cidade: \n");
+    scanf(" %s", cidade);
+
+    printf("Digite o Número da População: \n");
+    scanf("%d", &populacao);
+
+    printf("Digite a Área em km²: \n");
+    scanf("%f", &area_km2);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("Digite a Quantidade de Pontos Turísticos: \n");
+    scanf("%d", &pontos_turisticos);
+
+    printf("Estado: %s - Carta: %s - Cidade: %s\n", estado, carta, cidade);
+    printf("População: %d - Área: %.2f km²\n", populacao, area_km2);
+    printf("PIB: %.2f - Pontos Turísticos: %d\n", PIB, pontos_turisticos);
 
     return 0;
 }
